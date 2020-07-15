c144884284d3867ecc8f9248b4eda30094a2b300
Francisco Garcia <garcifran@hotmail.com>
2020-07-15 14:51:12 -0500

    Configuracion de base de datos

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict'
 
+require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
-const { readFileSync} = require('fs')
+const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
 
@@ -11,7 +12,7 @@ const app = express()
 const port = process.env.port || 4000
 
 // definiendo el esquema
-const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'),'utf8')
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', graphqlHTTP({
