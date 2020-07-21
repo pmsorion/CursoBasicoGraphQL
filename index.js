f472c083aebaf6d54ce097faab42f9c49fb1c167
Francisco Garcia <garcifran@hotmail.com>
2020-07-21 17:30:55 -0500

    commit final

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -10,15 +11,18 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 4000
+const isDev = process.env.NODE_ENV !== 'production'
 
 // definiendo el esquema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
+app.use(cors())
+
 app.use('/api', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
